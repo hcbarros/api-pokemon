a1253252aa8f3127a5ea724ba2199245f154567d
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-06-11 17:18:18 -0300

    first commit

---
+++ src/test/java/br/com/pokemon/ApiPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
